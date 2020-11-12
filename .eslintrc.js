5bd04494842200e5a2aff3d2d50a4fcd7cb8b85b
ym-project <ym-project@protonmail.com>
2020-11-12 14:12:33 +0800

    add linter

---
+++ .eslintrc.js
@@ -0,0 +1,80 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	plugins: [
+		'@typescript-eslint',
+	],
+	env: {
+		node: true,
+		es2021: true,
+	},
+	rules: {
+		// ignore rule because of @typescript-eslint/no-unused-vars
+		'no-unused-vars': 'off',
+
+		// disallow __proto__
+		'no-proto': 'error',
+		// disallow return await
+		'no-return-await': 'error',
+		// disallow eval
+		'no-eval': 'error',
+		// max line length
+		'max-len': [
+			'error',
+			{
+				code: 120,
+			},
+		],
+		// require empty line in the end of file
+		'eol-last': 'error',
+		// allow single quotes and backticks with ${}
+		'quotes': [
+			'error',
+			'single',
+			{
+				avoidEscape: true,
+			},
+		],
+		// disallow ;
+		'semi': [
+			'error',
+			'never',
+		],
+		// require trailing comma only when entity has multiline format
+		'comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'never',
+			},
+		],
+		// disallow func( 'arg' ), only func('arg')
+		'space-in-parens': [
+			'error',
+			'never',
+		],
+		// disallow if, for, ... without brackets
+		'curly': 'error',
+		// disallow empty block like if (...) {}. Allow only empty catch {}
+		'no-empty': [
+			'error',
+			{
+				allowEmptyCatch: true,
+			},
+		],
+		// allow tabs
+		'indent': [
+			'error',
+			'tab',
+		],
+	},
+}
