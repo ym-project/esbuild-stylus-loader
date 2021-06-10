d04ab4fd753d8dfbdb68a05813d5646979948b1b
ym-project <ym-project@protonmail.com>
2021-06-11 02:25:58 +0800

    chore: install commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'chore',
+				'feat',
+				'fix',
+				'docs',
+				'style',
+			],
+		],
+	},
+}
