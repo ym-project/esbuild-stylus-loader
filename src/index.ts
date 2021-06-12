7d0b7e95adb18ed40ec6c3021c336c8cc7e44226
ym-project <ym-project@protonmail.com>
2021-06-13 05:16:57 +0800

    fix: allow to use non-stylus files
    - @import css files
    - css url() keyword

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,82 @@
 import * as path from 'path'
 import {promises as fs} from 'fs'
 import {Plugin} from 'esbuild'
-import {LoaderOptions} from './types'
 import stylusToCss from './stylus-to-css'
 
-export function stylusLoader(options: LoaderOptions = {}): Plugin {
+interface PluginOptions {
+	url?: boolean
+}
+
+export function stylusLoader(pluginOptions: PluginOptions = {}): Plugin {
 	return {
 		name: 'stylus-loader',
 		setup(build) {
-			build.onResolve({filter: /\.styl$/}, args => ({
-				path: path.resolve(
-					process.cwd(),
-					path.relative(process.cwd(), args.resolveDir),
-					args.path
-				),
-				namespace: 'stylus',
-			}))
+			// intercept stylus files
+			build.onResolve({filter: /\.(styl|stylus)$/}, args => {
+				return {
+					path: path.resolve(
+						process.cwd(),
+						path.relative(process.cwd(), args.resolveDir),
+						args.path,
+					),
+					namespace: 'stylus',
+				}
+			})
+
+			// intercept non-css and non-stylus files
+			build.onResolve({filter: /.*/}, args => {
+				if (args.namespace !== 'stylus') {
+					return
+				}
+
+				if (pluginOptions.url === true) {
+					return {
+						path: path.resolve(
+							process.cwd(),
+							path.relative(process.cwd(), args.resolveDir),
+							args.path,
+						),
+						namespace: 'static',
+					}
+				}
+
+				return {
+					external: true,
+				}
+			})
 
 			build.onLoad({
 				filter: /.*/,
 				namespace: 'stylus',
 			}, async args => {
-				const stylusContent = await fs.readFile(args.path, 'utf-8')
-				const code = await stylusToCss(stylusContent, {
-					stylusOptions: options,
+				const content = await fs.readFile(args.path, 'utf8')
+				const code = await stylusToCss(content, {
 					filePath: args.path,
 				})
 
 				return {
 					contents: code,
 					loader: 'css',
+					resolveDir: path.dirname(args.path),
+				}
+			})
+
+			build.onLoad({
+				filter: /.*/,
+				namespace: 'static',
+			}, async args => {
+				const contents = await fs.readFile(args.path, 'utf8')
+
+				if (/\.css$/.test(args.path)) {
+					return {
+						contents,
+						loader: 'css',
+					}
+				}
+
+				return {
+					contents,
+					loader: 'file',
 				}
 			})
 		},
--- src/stylus-to-css.ts
+++ src/stylus-to-css.ts
@@ -1,48 +1,13 @@
 import stylus from 'stylus'
-import {
-	StylusToCssOptions,
-	StylusToCss,
-} from './types'
 
-export default function stylusToCss(content: string, options: StylusToCssOptions): StylusToCss {
+interface Options {
+	filePath: string
+}
+
+export default function stylusToCss(content: string, options: Options): Promise<string> {
 	return new Promise((resolve, reject) => {
-		const {
-			stylusOptions,
-			filePath,
-		} = options
 		const styl = stylus(content)
-
-		styl.set('filename', filePath)
-
-		if (stylusOptions.sourcemap) {
-			if (stylusOptions.sourcemap === 'inline') {
-				styl.set('sourcemap', {inline: true})
-			}
-		}
-
-		if (stylusOptions.use) {
-			stylusOptions.use.forEach(usage => {
-				styl.use(usage)
-			})
-		}
-
-		if (stylusOptions.import) {
-			stylusOptions.import.forEach(file => {
-				styl.import(file)
-			})
-		}
-
-		if (stylusOptions.include) {
-			stylusOptions.include.forEach(include => {
-				styl.include(include)
-			})
-		}
-
-		if (stylusOptions.define) {
-			stylusOptions.define.forEach(define => {
-				styl.define(define[0], define[1])
-			})
-		}
+		styl.set('filename', options.filePath)
 
 		styl.render((err, css) => {
 			if (err) {
